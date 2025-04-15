d8ce07dfd88b7fe79824ee69eef2ebd2d09d5e2c
patilatul-goog <patilatul@google.com>
2025-04-15 13:19:41 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	// Import the local greeting package using the module path + subdirectory
+	"example.com/hello/greeting" // Make sure 'example.com/hello' matches your go.mod module path
+
+	// Import a third-party package
+	"rsc.io/quote"
+)
+
+// main is the entry point of the program.
+func main() {
+	// Get a greeting from the local package
+	message := greeting.GetGreeting("Gladys")
+	fmt.Println(message)
+
+	// Get a quote from the third-party package
+	fmt.Println("And here's a Go proverb:")
+	fmt.Println(quote.Go())
+}
